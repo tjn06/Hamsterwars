12cc63b5a10352fc782a7d2f3936a801ba2cbfee
Arthur Oresjö <arthur_jul@hotmail.com>
2020-10-01 00:09:32 +0200

    header,add och home responsivt och med funktionalitet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,49 +3,65 @@ import ReactDOM from "react-dom";
 import { NavLink } from "react-router-dom";
 import "../styles/header.scoped.css";
 
-
 function Header() {
-
-  
-
+	function menu() {
+		let navWrapper = document.querySelector(".nav-wrapper"),
+			navToogler = document.querySelector(".nav-toogler");
+		navToogler.addEventListener("click", function(event) {
+			navWrapper.classList.toggle("active");
+		});
+	}
 	return (
-		<div class="nav-wrapper" >
-			<nav class="nav">
-				<div className="logo">
+		<div className="nav-wrapper" onClick={() => menu()}>
+			<nav className="nav">
+								<div className="logo">
 					<img src={require("../pics/headerlogo.svg")} alt="" />
 				</div>
-				<div class="navigation">
-					<div class="nav-toogler" />
-					<ul class="nav-items">
-						<NavLink
-							to="/"
-							className="toHome"
-							activeClassName="active"
-							end
-						>
-							Home
-						</NavLink>
-						<NavLink
-							to="/add"
-							className="toAdd"
-							activeClassName="active"
-						>
-							Add Hamster
-						</NavLink>
-						<NavLink
-							to="/stats"
-							className="toStats"
-							activeClassName="active"
-						>
-							Hamster stats
-						</NavLink>
-						<NavLink
-							to="/battlemode"
-							className="toBattlemode"
-							activeClassName="active"
-						>
-							Battlemode
-						</NavLink>
+				<div className="navigation">
+					<div className="nav-toogler" />
+					<ul className="nav-items">
+						<div className="flex-icons">
+							{" "}
+							<img src={require("../pics/home.svg")} alt="" />
+							<NavLink
+								to="/"
+								className="toHome"
+								activeClassName="active"
+								end
+							>
+								Home
+							</NavLink>
+						</div>
+						<div className="flex-icons">
+							<img src={require("../pics/add.svg")} alt="" />
+							<NavLink
+								to="/add"
+								className="toAdd"
+								activeClassName="active"
+							>
+								Add Hamster
+							</NavLink>
+						</div>
+						<div className="flex-icons">
+							<img src={require("../pics/stats.svg")} alt="" />
+							<NavLink
+								to="/stats"
+								className="toStats"
+								activeClassName="active"
+							>
+								Hamster stats
+							</NavLink>
+						</div>
+						<div className="flex-icons">
+            <img src={require("../pics/battle.svg")} alt="" />
+							<NavLink
+								to="/battlemode"
+								className="toBattlemode"
+								activeClassName="active"
+							>
+								Battlemode
+							</NavLink>
+						</div>
 					</ul>
 				</div>
 			</nav>
