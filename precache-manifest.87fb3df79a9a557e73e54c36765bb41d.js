7f4b7469b441e9ce376e06a59a68e48bc075068d
Tobias <tobb-on@hotmail.com>
2020-10-05 21:00:12 +0200

    Updates

---
+++ precache-manifest.87fb3df79a9a557e73e54c36765bb41d.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef8578e987ea1cff4b01f739ea07380",
+    "url": "/Hamsterwars/index.html"
+  },
+  {
+    "revision": "27a1977771498c6a74ca",
+    "url": "/Hamsterwars/static/css/main.2df1d040.chunk.css"
+  },
+  {
+    "revision": "51816491a86945243493",
+    "url": "/Hamsterwars/static/js/2.ce27789e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hamsterwars/static/js/2.ce27789e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a1977771498c6a74ca",
+    "url": "/Hamsterwars/static/js/main.945ceed0.chunk.js"
+  },
+  {
+    "revision": "243a41de777918eb367b",
+    "url": "/Hamsterwars/static/js/runtime-main.6cca1f30.js"
+  },
+  {
+    "revision": "32193880b7823604e1e4e16d2cce07f1",
+    "url": "/Hamsterwars/static/media/add.32193880.svg"
+  },
+  {
+    "revision": "75448686a287f7ca633cf6f3db2b6829",
+    "url": "/Hamsterwars/static/media/battle.75448686.svg"
+  },
+  {
+    "revision": "f7ad247d8549718d7e337a77e30b4a5e",
+    "url": "/Hamsterwars/static/media/desktop-background.f7ad247d.svg"
+  },
+  {
+    "revision": "7f35c84304deb96d86319a886c277501",
+    "url": "/Hamsterwars/static/media/hamster-1.7f35c843.jpg"
+  },
+  {
+    "revision": "5d160323d71375e8a7709192d8b16e51",
+    "url": "/Hamsterwars/static/media/hamster-10.5d160323.jpg"
+  },
+  {
+    "revision": "31556a4633945575470bbd2fe9e94e44",
+    "url": "/Hamsterwars/static/media/hamster-11.31556a46.jpg"
+  },
+  {
+    "revision": "e5cb8eeee2e6fadae826c72f4578b230",
+    "url": "/Hamsterwars/static/media/hamster-12.e5cb8eee.jpg"
+  },
+  {
+    "revision": "3efb741bc546c6a3d4b158a8638e6288",
+    "url": "/Hamsterwars/static/media/hamster-13.3efb741b.jpg"
+  },
+  {
+    "revision": "5af925dd3d2f56c2a916886fe26e6682",
+    "url": "/Hamsterwars/static/media/hamster-14.5af925dd.jpg"
+  },
+  {
+    "revision": "dae36dd91c32dc1f941645160e210368",
+    "url": "/Hamsterwars/static/media/hamster-15.dae36dd9.jpg"
+  },
+  {
+    "revision": "4b953d9fe27122ac0b6c40420aa3cc8a",
+    "url": "/Hamsterwars/static/media/hamster-16.4b953d9f.jpg"
+  },
+  {
+    "revision": "3258c0b2c36a1198062223a428b8a47e",
+    "url": "/Hamsterwars/static/media/hamster-18.3258c0b2.jpg"
+  },
+  {
+    "revision": "244b1a01a45bf29637d908c9bcad6c79",
+    "url": "/Hamsterwars/static/media/hamster-19.244b1a01.jpg"
+  },
+  {
+    "revision": "75d7c00c29d6e4e5f0be6b7ad9fd91d2",
+    "url": "/Hamsterwars/static/media/hamster-2.75d7c00c.jpg"
+  },
+  {
+    "revision": "2a90ecee65511de0e411643959bdd644",
+    "url": "/Hamsterwars/static/media/hamster-20.2a90ecee.jpg"
+  },
+  {
+    "revision": "d5192e66416b364637b246439be6b2ac",
+    "url": "/Hamsterwars/static/media/hamster-21.d5192e66.jpg"
+  },
+  {
+    "revision": "8c3d060dc634230b68bb83f879154eec",
+    "url": "/Hamsterwars/static/media/hamster-22.8c3d060d.jpg"
+  },
+  {
+    "revision": "6bdc73c9e03fac00131b2fbd98be4d47",
+    "url": "/Hamsterwars/static/media/hamster-24.6bdc73c9.jpg"
+  },
+  {
+    "revision": "01593b20f4b1d44fc96b7b06377741c0",
+    "url": "/Hamsterwars/static/media/hamster-25.01593b20.jpg"
+  },
+  {
+    "revision": "d54231572dd9301effc62f0b13449d46",
+    "url": "/Hamsterwars/static/media/hamster-26.d5423157.jpg"
+  },
+  {
+    "revision": "dd3a4a0de66c04a013477444d65e4ea3",
+    "url": "/Hamsterwars/static/media/hamster-27.dd3a4a0d.jpg"
+  },
+  {
+    "revision": "13a81351b3bbe856a8988fc97cbf23b6",
+    "url": "/Hamsterwars/static/media/hamster-28.13a81351.jpg"
+  },
+  {
+    "revision": "9533f2f30a18b4e3ab9587afbb29c4eb",
+    "url": "/Hamsterwars/static/media/hamster-29.9533f2f3.jpg"
+  },
+  {
+    "revision": "0c82879432fd6d08e2ba4297e8660e67",
+    "url": "/Hamsterwars/static/media/hamster-3.0c828794.jpg"
+  },
+  {
+    "revision": "8f20cd1d850a95e7b868892d5fdde817",
+    "url": "/Hamsterwars/static/media/hamster-30.8f20cd1d.jpg"
+  },
+  {
+    "revision": "e5b4100af7fac352f58661204561d67e",
+    "url": "/Hamsterwars/static/media/hamster-31.e5b4100a.jpg"
+  },
+  {
+    "revision": "b1edd9a6f154cbae9bdabfe3a68ad2bc",
+    "url": "/Hamsterwars/static/media/hamster-32.b1edd9a6.jpg"
+  },
+  {
+    "revision": "657f8af74c83e44537233b0527f7087a",
+    "url": "/Hamsterwars/static/media/hamster-33.657f8af7.jpg"
+  },
+  {
+    "revision": "4ec59ca12bca2e8f57c9a172e65e288e",
+    "url": "/Hamsterwars/static/media/hamster-34.4ec59ca1.jpg"
+  },
+  {
+    "revision": "aa3699a28177e4de1c6e27d5ad056125",
+    "url": "/Hamsterwars/static/media/hamster-35.aa3699a2.jpg"
+  },
+  {
+    "revision": "1eb094d74c1a53ffdd5d046157258e31",
+    "url": "/Hamsterwars/static/media/hamster-36.1eb094d7.jpg"
+  },
+  {
+    "revision": "716ae5708ced7382a1f0176625056c16",
+    "url": "/Hamsterwars/static/media/hamster-37.716ae570.jpg"
+  },
+  {
+    "revision": "523c075ad588f2742ca6f78a7fcfda8c",
+    "url": "/Hamsterwars/static/media/hamster-38.523c075a.jpg"
+  },
+  {
+    "revision": "788579e00949819d605ead90f07011c3",
+    "url": "/Hamsterwars/static/media/hamster-39.788579e0.jpg"
+  },
+  {
+    "revision": "8724cefbeb5eaf69c1aa3ce03046616b",
+    "url": "/Hamsterwars/static/media/hamster-4.8724cefb.jpg"
+  },
+  {
+    "revision": "ab5503e682c67361e1863ebdfaba3a97",
+    "url": "/Hamsterwars/static/media/hamster-40.ab5503e6.jpg"
+  },
+  {
+    "revision": "51ede4c7e8e84450b76575d94f7bddba",
+    "url": "/Hamsterwars/static/media/hamster-5.51ede4c7.jpg"
+  },
+  {
+    "revision": "c21e6fb4928028a733ec6012442ed285",
+    "url": "/Hamsterwars/static/media/hamster-6.c21e6fb4.jpg"
+  },
+  {
+    "revision": "93bfb122c36f2ebdd8b2ed7702ebe973",
+    "url": "/Hamsterwars/static/media/hamster-7.93bfb122.jpg"
+  },
+  {
+    "revision": "6b940b10d6b5c642081e65d1619840b2",
+    "url": "/Hamsterwars/static/media/hamster-8.6b940b10.jpg"
+  },
+  {
+    "revision": "0e5ee9f2f14d0bd7f92713fa2f3f43c7",
+    "url": "/Hamsterwars/static/media/hamster-9.0e5ee9f2.jpg"
+  },
+  {
+    "revision": "65aeb4c986b5b6dcccd7250cf88abaeb",
+    "url": "/Hamsterwars/static/media/headerlogo.65aeb4c9.svg"
+  },
+  {
+    "revision": "a4c02de9c5c3da38a327a0361e4248ad",
+    "url": "/Hamsterwars/static/media/home.a4c02de9.svg"
+  },
+  {
+    "revision": "024261e36679001bff4dba7968b2fdce",
+    "url": "/Hamsterwars/static/media/logo.024261e3.svg"
+  },
+  {
+    "revision": "52d214b1c8e48fda85fefb50bbf7113c",
+    "url": "/Hamsterwars/static/media/mobil-background.52d214b1.svg"
+  },
+  {
+    "revision": "eb8e4e6adb7d156b2be54470e0418981",
+    "url": "/Hamsterwars/static/media/stats.eb8e4e6a.svg"
+  }
+]);
